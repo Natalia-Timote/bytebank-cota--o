db7b27ca90042cea954b82848d8368f89e408460
Natalia-Timote <nataliamirian.nt@gmail.com>
2024-08-16 21:59:20 -0300

    incluindo data

--- script/scripts.js
+++ script/scripts.js
@@ -18,4 +18,13 @@ async function conectaAPI() {
   console.log(conectaTraduzido);
 }
 
-conectaAPI();
+setInterval(() => conectaAPI(), 5000);
+
+function geraHorario() {
+  let data = new Date();
+  let horario = data.getHours() + ':' + data.getMinutes() + ':' + data.getSeconds();
+  console.log(horario);
+  return horario;
+}
+
+geraHorario();
